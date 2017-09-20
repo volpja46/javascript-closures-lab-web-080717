469f509ddb8dd8e6508a96db3bf0b1333aa4057c
Jennifer Volpe <jennifervolpe@Jennifers-MacBook-Pro.local>
2017-09-20 17:34:56 -0400

    :done:

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-const app = "I don't do much."
+// const app = "I don't do much."
+
+function bumpCounter() {
+  let counter = 0
+
+function addBump(){
+  counter ++;
+  }
+
+function getBumps(){
+  return counter;
+  }
+// **addBump && getBumps need to be returned since they will be used to increase the counter and get the counter value.
+  return {
+  addBump,
+  getBumps,
+  };
+}
+
+function createAnimal(animalType) {
+  return function (deadlyDevice){
+    return { animalType, deadlyDevice };
+    // { animalType: 'Monkey', deadlyDevice: 'Lightsaber' }
+    // This object has two properties, animalType and deadlyDevice that are the given arguments to both functions.
+  }
+}
+
+var sharkCreator = createAnimal('Shark');
+var sharkWithFrickinLaserbeam = sharkCreator("Laserbeam")
+var sharkWithFrickinCannon = sharkCreator("Cannon")
